8e6ccd2201c47a318e4b832972a14770a8613f71
adnanmc <adnanmcj@gmail.com>
2020-03-06 17:10:39 -0500

    WIP validation
    changed the airport data generation script

--- generateAirportData.js
+++ generateAirportData.js
@@ -6,50 +6,32 @@ const openFlightDataURL =
 axios.get(openFlightDataURL).then(res => {
   let rawText = new Array(res.data.split("\n"));
   let data = [];
-  let airportCodes = [];
-  // console.log(rawText[0]);
-
   rawText[0].forEach(row => {
-    // console.log(row.toString().split(","));
     let rawRowData = row.toString().split(",");
-    // console.log(rawRowData[0]);
-
     let tempObject = {};
     tempObject["airportId"] = v(rawRowData[0]).trim('"');
     tempObject["name"] = v(rawRowData[1]).trim('"');
     tempObject["city"] = v(rawRowData[2]).trim('"');
     tempObject["country"] = v(rawRowData[3]).trim('"');
-    tempObject["IATA-code"] = v(rawRowData[4]).trim('"');
-    tempObject["ICAO-code"] = v(rawRowData[5]).trim('"');
+    tempObject["IATA"] = v(rawRowData[4]).trim('"');
+    tempObject["ICAO"] = v(rawRowData[5]).trim('"');
     tempObject["latitude"] = v(rawRowData[6]).trim('"');
     tempObject["longitude"] = v(rawRowData[7]).trim('"');
     tempObject["altitude"] = v(rawRowData[8]).trim('"');
     tempObject["timezone"] = v(rawRowData[9]).trim('"');
     tempObject["DST"] = v(rawRowData[10]).trim('"');
-    tempObject["tz-database-time-zone"] = v(rawRowData[11]).trim('"');
+    tempObject["tzOlson"] = v(rawRowData[11]).trim('"');
     tempObject["type"] = v(rawRowData[12]).trim('"');
     tempObject["source"] = v(rawRowData[13]).trim('"');
     data.push(tempObject);
-    if (!v(tempObject["IATA-code"]).matches(/\\\\N/)) {
-      airportCodes.push(tempObject["IATA-code"]);
-    }
   });
   let allDataFileName = "./utils/airportData.json";
-  let allAirportCodeFileName = "./utils/airportCodes.json";
   let finalData = JSON.stringify(data);
-  let finalAirportCodes = JSON.stringify(airportCodes);
   fs.writeFile(allDataFileName, finalData, err => {
     if (err) {
       console.log(err);
     } else {
       console.log(`File: ${allDataFileName} has been saved`);
-    }
-  });
-  fs.writeFile(allAirportCodeFileName, finalAirportCodes, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`File: ${allAirportCodeFileName} has been saved`);
     }
   });
 });
--- test2.js
+++ test2.js
@@ -3,20 +3,8 @@
 // let result = stgRegex.test(stg);
 // console.log(result);
 const fs = require("fs");
-const airporFile = "./utils/airportData.json";
-let fileData;
-fs.readFile(airporFile, (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    fileData = JSON.stringify(data);
-  }
+const airportFile = require("./utils/airportData.json");
+
+const airportCodesArray = airportFile.forEach(element => {
+  console.log(element);
 });
-// const airportDataJson = JSON.parse(fileData);
-
-// let airportCodeArray = airportDataJson.forEach(element => {
-//   return element["IATA-code"];
-// });
-
-console.log(fileData);
