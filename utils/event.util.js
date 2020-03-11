dab21c3373505d5abdfa0677dec36b1ac5780931
adnanmc <adnanmcj@gmail.com>
2020-03-11 13:24:44 -0400

    WIP send event file
    established code for OUT OFF ON and IN messages

--- utils/event.util.js
+++ utils/event.util.js
@@ -1,5 +1,5 @@
 const v = require("voca");
-const { getSendFolder, sendAdhocFile } = require("./common.util");
+const { sendAdhocFile } = require("./common.util");
 
 const sendAdhoc16 = async data => {
   let eventType = data.eventType;
@@ -14,6 +14,24 @@ const sendAdhoc16 = async data => {
     case "OUT":
       let outUTC = v(data.outUTC).padLeft(4, "0");
       let adhocString = `${adhoc16BaseString}${eventType}${outUTC}`;
+      let fileStatus = await sendAdhocFile(stg, eventType, adhocString);
+      return fileStatus;
+
+    case "OFF":
+      let offUTC = v(data.offUTC).padLeft(4, "0");
+      let adhocString = `${adhoc16BaseString}${eventType}9999${offUTC}`;
+      let fileStatus = await sendAdhocFile(stg, eventType, adhocString);
+      return fileStatus;
+
+    case "ON":
+      let onUTC = v(data.onUTC).padLeft(4, "0");
+      let adhocString = `${adhoc16BaseString}${eventType}_${onUTC}`;
+      let fileStatus = await sendAdhocFile(stg, eventType, adhocString);
+      return fileStatus;
+
+    case "IN":
+      let inUTC = v(data.inUTC).padLeft(4, "0");
+      let adhocString = `${adhoc16BaseString}${eventType}_9999${inUTC}`;
       let fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
