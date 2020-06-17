41c866e8c8197a390ae8c42abf434fb66ec3d510
adnanmc <adnanmcj@gmail.com>
2020-06-16 23:24:22 -0400

    added input for outUTC within OFF event and onUTC within IN event

--- utils/event.util.js
+++ utils/event.util.js
@@ -1,14 +1,14 @@
-const v = require("voca");
-const { sendAdhocFile } = require("./common.util");
+const v = require('voca');
+const { sendAdhocFile } = require('./common.util');
 
-const sendAdhoc16 = async data => {
+const sendAdhoc16 = async (data) => {
   let eventType = data.eventType;
   let stg = data.stg;
-  let flightNum = v(data.flightNum).padLeft(4, "0");
+  let flightNum = v(data.flightNum).padLeft(4, '0');
   let utcOriginDate = data.utcOriginDate;
   let origin = data.origin;
   let destination = data.destination;
-  let stdUTC = v(data.stdUTC).padLeft(4, "0");
+  let stdUTC = v(data.stdUTC).padLeft(4, '0');
   let adhoc16BaseString = `ADH016${flightNum}${utcOriginDate}${origin}${destination}${stdUTC}`;
   let outUTC;
   let offUTC;
@@ -27,142 +27,144 @@ const sendAdhoc16 = async data => {
   let adhocString;
   let fileStatus;
   switch (eventType) {
-    case "OUT":
-      outUTC = v(data.outUTC).padLeft(4, "0");
+    case 'OUT':
+      outUTC = v(data.outUTC).padLeft(4, '0');
       adhocString = `${adhoc16BaseString}${eventType}${outUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "OFF":
-      offUTC = v(data.offUTC).padLeft(4, "0");
-      adhocString = `${adhoc16BaseString}${eventType}9999${offUTC}`;
+    case 'OFF':
+      outUTC = v(data.outUTC).padLeft(4, '0');
+      offUTC = v(data.offUTC).padLeft(4, '0');
+      adhocString = `${adhoc16BaseString}${eventType}${outUTC}${offUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "ON":
-      onUTC = v(data.onUTC).padLeft(4, "0");
+    case 'ON':
+      onUTC = v(data.onUTC).padLeft(4, '0');
       adhocString = `${adhoc16BaseString}${eventType}_${onUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "IN":
-      inUTC = v(data.inUTC).padLeft(4, "0");
-      adhocString = `${adhoc16BaseString}${eventType}_9999${inUTC}`;
+    case 'IN':
+      onUTC = v(data.onUTC).padLeft(4, '0');
+      inUTC = v(data.inUTC).padLeft(4, '0');
+      adhocString = `${adhoc16BaseString}${eventType}_${onUTC}${inUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "ETD":
-      etdUTC = v(data.etdUTC).padLeft(4, "0");
+    case 'ETD':
+      etdUTC = v(data.etdUTC).padLeft(4, '0');
       adhocString = `${adhoc16BaseString}${eventType}${etdUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "ETA":
-      etaUTC = v(data.etaUTC).padLeft(4, "0");
+    case 'ETA':
+      etaUTC = v(data.etaUTC).padLeft(4, '0');
       adhocString = `${adhoc16BaseString}${eventType}${etaUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "ETO":
-      etoUTC = v(data.etoUTC).padLeft(4, "0");
+    case 'ETO':
+      etoUTC = v(data.etoUTC).padLeft(4, '0');
       adhocString = `${adhoc16BaseString}${eventType}${etoUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "EON":
-      eonUTC = v(data.eonUTC).padLeft(4, "0");
+    case 'EON':
+      eonUTC = v(data.eonUTC).padLeft(4, '0');
       adhocString = `${adhoc16BaseString}${eventType}${eonUTC}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "SUB":
-      tailNum = v(data.tailNum).padLeft(4, " ");
+    case 'SUB':
+      tailNum = v(data.tailNum).padLeft(4, ' ');
       adhocString = `${adhoc16BaseString}${eventType}${tailNum}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "ASN":
-      tailNum = v(data.tailNum).padLeft(4, " ");
+    case 'ASN':
+      tailNum = v(data.tailNum).padLeft(4, ' ');
       adhocString = `${adhoc16BaseString}${eventType}${tailNum}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "GTD":
-      depGate = v(data.depGate).padLeft(4, " ");
+    case 'GTD':
+      depGate = v(data.depGate).padLeft(4, ' ');
       adhocString = `${adhoc16BaseString}${eventType}${depGate}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "GTA":
-      arrGate = v(data.arrGate).padLeft(4, " ");
+    case 'GTA':
+      arrGate = v(data.arrGate).padLeft(4, ' ');
       adhocString = `${adhoc16BaseString}${eventType}${arrGate}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "DVC":
-      etaUTC = v(data.etaUTC).padLeft(4, "0");
+    case 'DVC':
+      etaUTC = v(data.etaUTC).padLeft(4, '0');
       divertCity = data.divertCity;
       adhocString = `${adhoc16BaseString}${eventType}${etaUTC}${divertCity}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "NEW":
-      staUTC = v(data.staUTC).padLeft(4, "0");
-      nextDayCrossover = "0";
+    case 'NEW':
+      staUTC = v(data.staUTC).padLeft(4, '0');
+      nextDayCrossover = '0';
       if (data.nextDayCrossover === true) {
-        nextDayCrossover = "1";
+        nextDayCrossover = '1';
       }
-      tailNum = v(data.tailNum).padLeft(4, " ");
+      tailNum = v(data.tailNum).padLeft(4, ' ');
       adhocString = `${adhoc16BaseString}${eventType}${staUTC}${nextDayCrossover}${tailNum}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "RIN":
+    case 'RIN':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "REM":
+    case 'REM':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "UDD":
+    case 'UDD':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "UDA":
+    case 'UDA':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "RMD":
+    case 'RMD':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "RMA":
+    case 'RMA':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "GRD":
+    case 'GRD':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "AIR":
+    case 'AIR':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "CNL":
+    case 'CNL':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
 
-    case "DEL":
+    case 'DEL':
       adhocString = `${adhoc16BaseString}${eventType}`;
       fileStatus = await sendAdhocFile(stg, eventType, adhocString);
       return fileStatus;
@@ -173,5 +175,5 @@ const sendAdhoc16 = async data => {
 };
 
 module.exports = {
-  sendAdhoc16
+  sendAdhoc16,
 };
--- utils/eventValidator.js
+++ utils/eventValidator.js
@@ -1,35 +1,35 @@
-const joi = require("@hapi/joi");
-const v = require("voca");
-const airportDataFile = require("./airportData.json");
+const joi = require('@hapi/joi');
+const v = require('voca');
+const airportDataFile = require('./airportData.json');
 const airportCodesArray = [];
-airportDataFile.forEach(element => {
+airportDataFile.forEach((element) => {
   airportCodesArray.push(element.IATA);
 });
 const allowedEvents = [
-  "OUT",
-  "OFF",
-  "ON",
-  "IN",
-  "ETD",
-  "ETA",
-  "ETO",
-  "EON",
-  "SUB",
-  "CNL",
-  "RIN",
-  "DEL",
-  "GTD",
-  "GTA",
-  "ASN",
-  "REM",
-  "UDD",
-  "UDA",
-  "RMD",
-  "RMA",
-  "GRD",
-  "AIR",
-  "DVC",
-  "NEW"
+  'OUT',
+  'OFF',
+  'ON',
+  'IN',
+  'ETD',
+  'ETA',
+  'ETO',
+  'EON',
+  'SUB',
+  'CNL',
+  'RIN',
+  'DEL',
+  'GTD',
+  'GTA',
+  'ASN',
+  'REM',
+  'UDD',
+  'UDA',
+  'RMD',
+  'RMA',
+  'GRD',
+  'AIR',
+  'DVC',
+  'NEW',
 ];
 const adhoc16Schema = joi.object({
   stg: joi
@@ -38,7 +38,7 @@ const adhoc16Schema = joi.object({
     .required()
     .pattern(/^stg[1-3]$/)
     .messages({
-      "string.pattern.base": '"stg" must be stg1 or stg2 or stg3'
+      'string.pattern.base': '"stg" must be stg1 or stg2 or stg3',
     }),
   flightNum: joi
     .string()
@@ -46,7 +46,7 @@ const adhoc16Schema = joi.object({
     .required()
     .pattern(/^\d{3}[1-9]$/)
     .messages({
-      "string.pattern.base": '"flightNum" must be 4 digit. Ex: 0024'
+      'string.pattern.base': '"flightNum" must be 4 digit. Ex: 0024',
     }),
   utcOriginDate: joi
     .string()
@@ -54,7 +54,7 @@ const adhoc16Schema = joi.object({
     .required()
     .pattern(/([12]\d{3}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01]))/)
     .messages({
-      "string.pattern.base": '"utcOriginDate" must be in the format YYYYMMDD'
+      'string.pattern.base': '"utcOriginDate" must be in the format YYYYMMDD',
     }),
   origin: joi
     .string()
@@ -63,7 +63,7 @@ const adhoc16Schema = joi.object({
     .uppercase()
     .valid(...airportCodesArray)
     .messages({
-      "any.only": '"origin" must be a valid IATA airport code'
+      'any.only': '"origin" must be a valid IATA airport code',
     }),
   destination: joi
     .string()
@@ -72,7 +72,7 @@ const adhoc16Schema = joi.object({
     .uppercase()
     .valid(...airportCodesArray)
     .messages({
-      "any.only": '"destination" must be a valid IATA airport code'
+      'any.only': '"destination" must be a valid IATA airport code',
     }),
   stdUTC: joi
     .string()
@@ -80,20 +80,20 @@ const adhoc16Schema = joi.object({
     .required()
     .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
     .messages({
-      "string.pattern.base":
-        '"stdUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
+      'string.pattern.base':
+        '"stdUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
     }),
-  staUTC: joi.when("eventType", {
-    is: "NEW",
+  staUTC: joi.when('eventType', {
+    is: 'NEW',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"staUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"staUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
   eventType: joi
     .string()
@@ -101,115 +101,139 @@ const adhoc16Schema = joi.object({
     .required()
     .uppercase()
     .valid(...allowedEvents),
-  outUTC: joi.when("eventType", {
-    is: "OUT",
+  outUTC: joi.when('eventType', {
+    is: 'OUT',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"outUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"outUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  offUTC: joi.when("eventType", {
-    is: "OFF",
+  outUTC: joi.when('eventType', {
+    is: 'OFF',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"offUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"outUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  onUTC: joi.when("eventType", {
-    is: "ON",
+  offUTC: joi.when('eventType', {
+    is: 'OFF',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"onUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"offUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  inUTC: joi.when("eventType", {
-    is: "IN",
+  onUTC: joi.when('eventType', {
+    is: 'ON',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"inUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"onUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  etdUTC: joi.when("eventType", {
-    is: "ETD",
+  onUTC: joi.when('eventType', {
+    is: 'IN',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"etdUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"onUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  etaUTC: joi.when("eventType", {
-    is: joi.valid(...["ETA", "DVC"]),
+  inUTC: joi.when('eventType', {
+    is: 'IN',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"etaUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"inUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  etoUTC: joi.when("eventType", {
-    is: "ETO",
+  etdUTC: joi.when('eventType', {
+    is: 'ETD',
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"etoUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"etdUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  eonUTC: joi.when("eventType", {
-    is: "EON",
+  etaUTC: joi.when('eventType', {
+    is: joi.valid(...['ETA', 'DVC']),
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
       .messages({
-        "string.pattern.base":
-          '"eonUTC" must be HHMM in 24hour format. Ex: 0059, 1523'
-      })
+        'string.pattern.base':
+          '"etaUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
   }),
-  tailNum: joi.when("eventType", {
-    is: joi.valid(...["SUB", "ASN", "NEW"]),
+  etoUTC: joi.when('eventType', {
+    is: 'ETO',
+    then: joi
+      .string()
+      .trim()
+      .required()
+      .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
+      .messages({
+        'string.pattern.base':
+          '"etoUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
+  }),
+  eonUTC: joi.when('eventType', {
+    is: 'EON',
+    then: joi
+      .string()
+      .trim()
+      .required()
+      .pattern(/^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/)
+      .messages({
+        'string.pattern.base':
+          '"eonUTC" must be HHMM in 24hour format. Ex: 0059, 1523',
+      }),
+  }),
+  tailNum: joi.when('eventType', {
+    is: joi.valid(...['SUB', 'ASN', 'NEW']),
     then: joi
       .string()
       .trim()
       .required()
       .pattern(/^[1-9]\d{2,3}$/)
       .messages({
-        "string.pattern.base": '"tailNum" must be 3 or 4 digit. Ex: 548'
-      })
+        'string.pattern.base': '"tailNum" must be 3 or 4 digit. Ex: 548',
+      }),
   }),
-  depGate: joi.when("eventType", {
-    is: "GTD",
+  depGate: joi.when('eventType', {
+    is: 'GTD',
     then: joi
       .string()
       .required()
@@ -217,12 +241,12 @@ const adhoc16Schema = joi.object({
       .uppercase()
       .regex(/^([a-zA-Z0-9]){1,4}$/)
       .messages({
-        "string.pattern.base":
-          '"depGate" must minimum 1 char max 4 char. Ex: 02A'
-      })
+        'string.pattern.base':
+          '"depGate" must minimum 1 char max 4 char. Ex: 02A',
+      }),
   }),
-  arrGate: joi.when("eventType", {
-    is: "GTA",
+  arrGate: joi.when('eventType', {
+    is: 'GTA',
     then: joi
       .string()
       .required()
@@ -230,12 +254,12 @@ const adhoc16Schema = joi.object({
       .uppercase()
       .regex(/^([a-zA-Z0-9]){1,4}$/)
       .messages({
-        "string.pattern.base":
-          '"arrGate" must minimum 1 char max 4 char. Ex: 02A'
-      })
+        'string.pattern.base':
+          '"arrGate" must minimum 1 char max 4 char. Ex: 02A',
+      }),
   }),
-  divertCity: joi.when("eventType", {
-    is: "DVC",
+  divertCity: joi.when('eventType', {
+    is: 'DVC',
     then: joi
       .string()
       .trim()
@@ -243,21 +267,18 @@ const adhoc16Schema = joi.object({
       .uppercase()
       .valid(...airportCodesArray)
       .messages({
-        "any.only": '"divertCity" must be a valid IATA airport code'
-      })
+        'any.only': '"divertCity" must be a valid IATA airport code',
+      }),
   }),
-  nextDayCrossover: joi.when("eventType", {
-    is: "NEW",
-    then: joi
-      .boolean()
-      .required()
-      .messages({
-        "boolean.base": '"nextDayCrossover" must be true or false'
-      })
-  })
+  nextDayCrossover: joi.when('eventType', {
+    is: 'NEW',
+    then: joi.boolean().required().messages({
+      'boolean.base': '"nextDayCrossover" must be true or false',
+    }),
+  }),
 });
 
-const adhoc16SchemaValidation = body => {
+const adhoc16SchemaValidation = (body) => {
   const { error, value } = adhoc16Schema.validate(body);
   if (error) {
     let userFriendlyErrorMessage = v(error.details[0].message).replaceAll(
@@ -271,5 +292,5 @@ const adhoc16SchemaValidation = body => {
 };
 
 module.exports = {
-  adhoc16SchemaValidation
+  adhoc16SchemaValidation,
 };
