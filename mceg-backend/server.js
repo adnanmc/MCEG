200f3e070c3c6a37ce13e6f41f05d8bd77d1b509
adnanmc <adnanmcj@gmail.com>
2021-04-21 10:09:57 -0400

    add temporary page before UI is complete

--- mceg-backend/server.js
+++ mceg-backend/server.js
@@ -26,6 +26,26 @@ app.use('/api/v1/events', eventRoutes);
 // event route
 app.use('/swagger', express.static('swagger'));
 
+// event route
+app.use('/', (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html>
+      <head>
+          
+          <title>MCEG</title>
+      </head>
+  <body style="text-align: center;">
+  <div style="margin: 0 auto;">
+      <div>UI is coming soon plese use the swagger link below for now</div>
+      
+      <button style="background-color: palegreen; margin: 10px;"><a href=/swagger>Swagger</a></button>
+  </div>    
+
+  </body>
+</html>`);
+});
+
 // stg1 route
 // app.use("/api/v1/flights", flightDataRoutes);
 
